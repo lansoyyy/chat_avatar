adee8c9d257a89491000a61c10387ead000568d8
Lans12345 <olanalans12345@gmail.com>
2024-07-25 07:54:23 +0800

    initial commit

---
+++ lib/const.dart
@@ -0,0 +1,5 @@
+String appId = '9a0dca27513e41109532906265b1d00e';
+
+String token =
+    '007eJxTYAhWYbHTuiIp8eVvL1/HjieVjb9utb/e5xW1WPnAPHNL5y8KDJaJBinJiUbmpobGqSaGhgaWpsZGlgZmRmamSYYpBgapLBMWpjUEMjL8e7GalZEBAkF8XobixNyCnNTkjMS8vNQcBgYAenojcQ==';
+String channelName = 'samplechannel';
